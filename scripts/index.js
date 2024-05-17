5ff4c7e2ffbd9d975429c1fe10e18986db533468
Eric Schwartz <erics273@gmail.com>
2024-05-17 14:47:54 -0400

    basic structure

---
+++ scripts/index.js
@@ -0,0 +1,18 @@
+"use strict"
+
+/*
+bootstrap card example:
+
+<div class="card" style="width: 18rem;">
+  <img src="PET IMAGE PATH GOES HERE" class="card-img-top" alt="ALT TEXT HERE">
+  <div class="card-body">
+    <h5 class="card-title">PET NAME HERE</h5>
+    <p class="card-text">PET DETAILS HERE</p>
+  </div>
+</div>
+*/
+
+window.onload = () => {
+
+
+}
