2a9103bd1406ad555e8d98ccfc4cdcb36baac622
Eric Schwartz <erics273@gmail.com>
2024-05-21 14:56:51 -0400

    completed

--- scripts/index.js
+++ scripts/index.js
@@ -18,6 +18,96 @@ window.onload = () => {
 
   let petDropdown = document.querySelector("#petSelect");
 
+  //we want to display the selected pet when the dropdown is chagned
+  petDropdown.addEventListener("change", displayPetCard)
+
+
+}
+
+function displayPetCard(event){
+
+  //get hold of the div where the pet will be dispayed
+  let petDiv = document.querySelector("#pets");
+
+  //clears out the prevous pet
+  petDiv.innerHTML = "";
+
+  //get the selected value from the dropdown
+  // let selectedPet = event.target.value;
+
+  // //find the matching pet in the pets array
+  // let pet = pets.find((pet)=>{
+    
+  //   if(pet.name === selectedPet){
+  //     return true;
+  //   }
+  //   return false;
+
+  // })
+
+  //one liner for the above
+  let pet = pets.find((pet) => pet.name === event.target.value)
+
+/*
+bootstrap card example:
+
+<div class="card w-50">
+  <img src="PET IMAGE PATH GOES HERE" class="card-img-top" alt="ALT TEXT HERE">
+  <div class="card-body">
+    <h5 class="card-title">PET NAME HERE</h5>
+    <p class="card-text">PET DETAILS HERE</p>
+  </div>
+</div>
+*/
+
+//lets create card div and add it's classes
+let cardDiv = document.createElement("div");
+cardDiv.classList.add("card", "w-25");
+
+//lets create the image and set it properties
+let cardImage = document.createElement("img");
+cardImage.classList.add("card-img-top", "card-img-fit");
+
+//add the src attribute to the image
+cardImage.src = pet.image;
+cardImage.alt = pet.name;
+
+//add the image to the cardDiv
+cardDiv.appendChild(cardImage);
+
+//create the card body and add it's classes
+let cardBody = document.createElement("div")
+cardDiv.classList.add("card-body");
+
+//lets create the card title
+let cardTitle = document.createElement("h5");
+cardTitle.classList.add("card-title");
+
+//set the pet name as the card title
+cardTitle.innerText = pet.name;
+
+
+//add the card title to the card body
+cardBody.appendChild(cardTitle);
+
+
+//crate the p tag for the card details
+let cardText = document.createElement("p");
+cardTitle.classList.add("card-text");
+
+cardText.innerHTML = `
+      ${pet.name} is a ${pet.type}. The breed is ${pet.breed} and their best trick is, ${pet.bestTrick}.
+    `
+
+//add the cardText to the cardBody
+cardBody.appendChild(cardText);
+
+//add the card body to the card div
+cardDiv.appendChild(cardBody)
+
+//add the card to the petDiv
+petDiv.appendChild(cardDiv)
+
 
 }
 
--- scripts/pets.js
+++ scripts/pets.js
@@ -4,42 +4,42 @@ let pets = [
       type: "Dog",
       breed: "Corgi",
       bestTrick: "Tug of war",
-      image: "images/rubby.com"
+      image: "images/rubby.jpg"
    }, {
       name: "Howdy",
       type: "Dog",
       breed: "Mixed Breed",
       bestTrick: "Go find it!",
-      image: "images/howdy.com"
+      image: "images/howdy.jpg"
    }, {
       name: "KitKit",
       type: "Cat",
       breed: "American Shorthair",
       bestTrick: "Commanding his owner to feed him",
-      image: "images/kitkit.com"
+      image: "images/kitkit.jpg"
    }, {
       name: "Lil' Miss",
       type: "Cat",
       breed: "Tabby",
       bestTrick: "Looking aloof",
-      image: "images/lilmiss.com"
+      image: "images/lilmiss.jpg"
    }, {
       name: "Happy",
       type: "Dog",
       breed: "Golden Retriever",
       bestTrick: "Refusing to retrieve!",
-      image: "images/happy.com"
+      image: "images/happy.jpg"
    }, {
       name: "Piper",
       type: "Dog",
       breed: "Beagle",
       bestTrick: "Find it!  Dropped food edition!",
-      image: "images/piper.com"
+      image: "images/piper.jpg"
    }, {
       name: "Spooky",
       type: "Cat",
       breed: "Mixed",
       bestTrick: "Gymnastics!",
-      image: "images/spooky.com"
+      image: "images/spooky.jpg"
    }
 ];
