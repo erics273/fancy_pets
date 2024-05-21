797308dd08336cdc356425996a67e9f9b2db6b1e
Eric Schwartz <erics273@gmail.com>
2024-05-21 13:37:08 -0400

    dropdown code

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,7 @@
 /*
 bootstrap card example:
 
-<div class="card" style="width: 18rem;">
+<div class="card">
   <img src="PET IMAGE PATH GOES HERE" class="card-img-top" alt="ALT TEXT HERE">
   <div class="card-body">
     <h5 class="card-title">PET NAME HERE</h5>
@@ -14,5 +14,45 @@ bootstrap card example:
 
 window.onload = () => {
 
+  initPetsDropdown();
+
+  let petDropdown = document.querySelector("#petSelect");
+
+
+}
+
+function initPetsDropdown() {
+    
+  //let's get a hold of the dropdown for the categories from the activities.html
+  let petDropdown = document.querySelector("#petSelect");
+
+  //create the element for the default option
+  let defaultOption = document.createElement("option");
+
+  //this is whate we get back in JS when we ask for the selected value
+  defaultOption.value = "";
+
+  //this is what the user actually selects from the dropdown
+  defaultOption.textContent = "Select a Pet";
+
+  //add the option we created to the dropdown
+  petDropdown.appendChild(defaultOption);
+
+  //write a loop to work with each individual category and build an option for it
+  pets.forEach( (pet) => {
+
+      //create the new option for the category we are on in the loop
+      let newOption = document.createElement("option");
+
+      //set the value for the option
+      newOption.value = pet.name;
+
+      //set the textContent that the user will see when choosing a category
+      newOption.textContent = pet.name;
+
+      petDropdown.appendChild(newOption);
+
+  })
+  
 
 }
